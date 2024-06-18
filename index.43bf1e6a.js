999fe2f8f22d37082e4cb6e8bd8f143c54d214ab
maxexc <104863585+maxexc@users.noreply.github.com>
2024-06-18 23:21:50 +0000

    Deploying to gh-pages from @ maxexc/Parcel-js@5daa14420570b9bebe08891007b7bc1ab5fdf777 🚀

--- index.43bf1e6a.js
+++ index.43bf1e6a.js
@@ -1,6 +1,6 @@
-let t=0,e=0,n=0;const o=()=>{let o=t+e+n,d=t/o*100||0,l=e/o*100||0,c=n/o*100||0,u=document.getElementById("diagram"),a=document.querySelector(".diagram-container"),r=document.querySelector(".notice");o>0&&(r.style.opacity=0,setTimeout(()=>{r.style.display="none",a.classList.add("show")},500)),u.style.setProperty("background",`conic-gradient(
+let t=0,e=0,n=0;const o=()=>{let o=t+e+n,d=t/o*100||0,l=e/o*100||0,c=n/o*100||0,a=document.getElementById("diagram"),u=document.querySelector(".diagram-container"),r=document.querySelector(".notice");o>0&&(r.style.opacity=0,r.style.display="none",u.classList.add("show")),a.style.setProperty("background",`conic-gradient(
             #3f8125 0% ${d}%, 
             orange ${d}% ${d+l}%, 
             red ${d+l}% ${d+l+c}%, 
             transparent ${d+l+c}% 100%)`),document.getElementById("total").textContent=o,document.getElementById("goodCount").textContent=t,document.getElementById("neutralCount").textContent=e,document.getElementById("badCount").textContent=n,document.getElementById("totalCount").textContent=o,document.getElementById("positivePercentage").textContent=`${d.toFixed(1)}%`,document.getElementById("percentageText").textContent=`${d.toFixed(1)}%`,document.getElementById("heartFill").style.transform=`scaleY(${d/100})`};document.querySelector(".feedback-buttons").addEventListener("click",d=>{let l=d.target.closest("button");l&&("likeButton"===l.id?t++:"neutralButton"===l.id?e++:"dislikeButton"===l.id&&n++,o())}),o();
-//# sourceMappingURL=index.256a8ab1.js.map
+//# sourceMappingURL=index.43bf1e6a.js.map
