efe776c9588fa009b5440d36b99d9978d2df4844
Maksym Solomko <max.exc7@gmail.com>
2024-06-18 14:04:48 +0200

    Card for feedback, Joj, server
    New Card for real-time feedback, library Joi, enquiry preparation

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import validatePassword from './js/validate-password';
+import apiServer from './js/api-server';
+
+console.log(validatePassword);
+
+console.log(validatePassword('oiuiouhuhuh'));
+
+console.log(apiServer["updateUserById"]);
+console.log(apiServer.fetchAllUsers);
